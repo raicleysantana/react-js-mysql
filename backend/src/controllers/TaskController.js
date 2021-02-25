31e7a4417cc31d38a76776e32edca122f5ba504f
raicleysantana <raicleysanana1@gmail.com>
2021-02-25 00:04:18 -0400

    Adicionado funçoes de crud

--- backend/src/controllers/TaskController.js
+++ backend/src/controllers/TaskController.js
@@ -15,7 +15,61 @@ class TaskController {
                 console.log(error);
             });
 
-            return true;
+        return true;
+    }
+
+    listarTarefa(req, res) {
+        database
+            .select("*")
+            .table("talks")
+            .then(tarefa => {
+                return tarefa.json(tarefa);
+            }).catch(error => {
+                console.log(error);
+            });
+    }
+
+    listarUmaTarefa(req, res) {
+        const id = req.params;
+
+        database
+            .select("*")
+            .table("talks")
+            .where({ id: id })
+            .then(tarefa => {
+                return tarefa.json(tarefa);
+            }).catch(error => {
+                console.log(error);
+            });
+    }
+
+    atualizarTarefa(req, res) {
+        const id = req.params;
+        const { descricao } = req.body;
+
+        database
+            .where({ id: id })
+            .update({ descricao: descricao })
+            .table("tasks")
+            .then(data => {
+                return res.json({ message: "Tarefa atualizada com sucesso" });
+            }).catch(error => {
+                return res.json(error);
+            });
+    }
+
+    removerTarefa(req, res) {
+        const id = req.params;
+
+        database
+            .where({ id: id })
+            .del()
+            .table("tasks")
+            .then(data => {
+                return res.json({ message: "Tarefa removovida com sucesso" });
+            }).catch(error => {
+                return res.json(error);
+            })
     }
 }
 
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const TaskController = require('../controllers/TaskController');
 
+router.get('/tarefas', TaskController.listarTarefa);
+router.get('/tarefas/:id', TaskController.listarUmaTarefa);
 router.post('/novaTarefa', TaskController.novaTarefa);
+router.put('/atualizar/tarefa/:id', TaskController.atualizarTarefa);
+router.delete('/delete/tarefa/:id', TaskController.removerTarefa);
 
 module.exports = router;
